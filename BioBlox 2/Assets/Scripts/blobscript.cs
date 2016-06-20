d9081d84627b9fd46735eb8d018179c3e6e8977e
Federico Soncini <fsonc002@gold.ac.uk>
2016-06-20 15:56:49 +0100

    added tags

--- BioBlox 2/Assets/Scripts/blobscript.cs
+++ BioBlox 2/Assets/Scripts/blobscript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class blobscript : MonoBehaviour {
 
+    //private bool isBall;
+    //private bool isBrick;
+
     void OnMouseDrag() {
         // calculate the mouse position in world space.
         float camera_distance = 10;
@@ -10,10 +13,43 @@ public class blobscript : MonoBehaviour {
         Camera cam = GameObject.FindObjectOfType<Camera>();
         Vector3 mouse_pos = cam.ScreenToWorldPoint(new Vector3(mp.x, mp.y, camera_distance));
         Vector3 delta = mouse_pos - transform.position;
+        //isBall = (this.tag == "ball");
+        //isBrick = (this.tag == "brick");
 
         // move the blob towards the mouse.
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         float spring_constant = 100;
         rb.AddForce(new Vector2(delta.x * spring_constant, delta.y * spring_constant));
     }
+
+
+    //void OnCollisionEnter2D()
+    //{
+    //    if(isBall)
+    //    {
+    //        Debug.Log("BALL COLLISION DETECTED");
+    //    }
+
+    //    if (isBrick)
+    //    {
+    //        Debug.Log("BRICK COLLISION DETECTED");
+    //    }
+
+    //}
+
+
+    void OnCircleCollisionEnter2D()
+    {
+   
+            Debug.Log("BALL COLLISION DETECTED");
+    
+    }
+
+    void OnBoxCollisionEnter2D()
+    {
+        
+            Debug.Log("BRICK COLLISION DETECTED");
+        
+    }
+
 }
