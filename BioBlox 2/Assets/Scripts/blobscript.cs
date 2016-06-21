62bd3fecb085c6a20e7bffa18f94f1992f723122
Elio de Berardinis <eliodeb@gmail.com>
2016-06-21 13:19:50 +0100

    getting bricks to "lock"

--- BioBlox 2/Assets/Scripts/blobscript.cs
+++ BioBlox 2/Assets/Scripts/blobscript.cs
@@ -3,6 +3,24 @@ using System.Collections;
 
 public class blobscript : MonoBehaviour {
 
+    public GameObject springsObject;
+    public springs springScript;
+    blobscript OtherBlob;
+
+    public bool collided;
+
+    GameObject thisItem;
+
+    void Start() {
+
+        collided = false;
+
+        thisItem = this.gameObject;
+        springsObject = GameObject.FindGameObjectWithTag("spring");
+        springScript = springsObject.GetComponent<springs>();
+
+    }
+
     void OnMouseDrag() {
         // calculate the mouse position in world space.
         float camera_distance = 10;
@@ -17,10 +35,31 @@ public class blobscript : MonoBehaviour {
        // rb.AddForce(new Vector2(delta.x * spring_constant, delta.y * spring_constant));
 
         transform.position = mouse_pos;
+        transform.rotation = Quaternion.identity;
     }
 
     void OnMouseDown() {
 
-        transform.Rotate(0,0,45);
+        //transform.Rotate(0,0,45);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+
+        if (coll.gameObject.name != "BottomWall" && coll.gameObject.name != "LeftWall" && !collided)
+        {
+            
+            springScript.balls.Add(thisItem);
+            collided = true;
+
+            OtherBlob = coll.gameObject.GetComponent<blobscript>();
+            if (!OtherBlob.collided)
+            {
+                springScript.balls.Add(coll.gameObject);
+                OtherBlob.collided = true;
+
+            }
+
+            
+        }
     }
 }
