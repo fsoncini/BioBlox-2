d978a49f1d4b9ab5ac60ecaf65e2dfb08a75af03
Federico Soncini <fsonc002@gold.ac.uk>
2016-06-21 12:49:06 +0100

    Added CountChildren( ) function

--- BioBlox 2/Assets/Scripts/Docks.cs
+++ BioBlox 2/Assets/Scripts/Docks.cs
@@ -6,4 +6,30 @@ public class Docks : MonoBehaviour {
 
     public List<GameObject> docks;
 
+    void Update ()
+    {
+        CountChildren();
+    }
+
+
+    void CountChildren ()
+    {
+        foreach (Transform child in transform)
+        {
+            Debug.Log("Foreach loop: " + child);
+            docks.Add(child.gameObject);
+        }
+
+        //{
+        //    int children = transform.childCount;
+        //    for (int i = 0; i < children; ++i)
+        //    {
+        //        Debug.Log("For loop: " + transform.GetChild(i));
+        //        docks.Add(GameObject.Find("Docks").);
+        //    }
+
+        //}
+
+    }
+
 }
