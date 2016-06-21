c95b397722986885e7b2370c8994819e0f6d41c8
Elio de Berardinis <eliodeb@gmail.com>
2016-06-21 16:13:13 +0100

    more adjustments

--- BioBlox 2/Assets/Scripts/Spawner.cs
+++ BioBlox 2/Assets/Scripts/Spawner.cs
@@ -7,10 +7,13 @@ public class Spawner : MonoBehaviour {
     public GameObject blob;
     GameObject Ribosome;
 
+    public int numSpawnItems;
+
 	// Use this for initialization
 	void Start () {
 
         Ribosome = GameObject.FindGameObjectWithTag("Ribo");
+        numSpawnItems = 0;
     }
 
     // Update is called once per frame
@@ -19,11 +22,13 @@ public class Spawner : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(brick, Ribosome.transform.position, Quaternion.identity);
+            numSpawnItems++;
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Instantiate(blob, Ribosome.transform.position, Quaternion.identity);
+            numSpawnItems++;
         }
     }
     }
--- BioBlox 2/Assets/Scripts/blobscript.cs
+++ BioBlox 2/Assets/Scripts/blobscript.cs
@@ -3,21 +3,23 @@ using System.Collections;
 
 public class blobscript : MonoBehaviour {
 
-    public GameObject springsObject;
-    public springs springScript;
+    springs springScript;
     blobscript OtherBlob;
-
+    Spawner _Spawner;
     public bool collided;
+
+    float natLength;
 
     GameObject thisItem;
 
     void Start() {
 
         collided = false;
+        natLength = 0.3f;
 
         thisItem = this.gameObject;
-        springsObject = GameObject.FindGameObjectWithTag("spring");
-        springScript = springsObject.GetComponent<springs>();
+        springScript = GameObject.FindGameObjectWithTag("spring").GetComponent<springs>();
+        _Spawner = GameObject.FindGameObjectWithTag("spawner").GetComponent<Spawner>();
 
     }
 
@@ -35,31 +37,37 @@ public class blobscript : MonoBehaviour {
        // rb.AddForce(new Vector2(delta.x * spring_constant, delta.y * spring_constant));
 
         transform.position = mouse_pos;
-        transform.rotation = Quaternion.identity;
+        transform.rotation = Quaternion.identity; //Check this
     }
 
     void OnMouseDown() {
 
-        //transform.Rotate(0,0,45);
+        //transform.Rotate(0,0,45); //Improve this
     }
 
     void OnCollisionEnter2D(Collision2D coll) {
 
         if (coll.gameObject.name != "BottomWall" && coll.gameObject.name != "LeftWall" && !collided)
         {
-            
+
+            natLength = (coll.transform.position - this.transform.position).magnitude;
+
             springScript.balls.Add(thisItem);
             collided = true;
 
-            OtherBlob = coll.gameObject.GetComponent<blobscript>();
-            if (!OtherBlob.collided)
+            for (int i = 0; i < _Spawner.numSpawnItems - 1; ++i)
             {
-                springScript.balls.Add(coll.gameObject);
-                OtherBlob.collided = true;
-
+                springScript.natural_length.Add(natLength); //Modify this (not precise)
             }
 
-            
+            //OtherBlob = coll.gameObject.GetComponent<blobscript>();
+            //if (!OtherBlob.collided)
+            //{
+            //    springScript.balls.Add(coll.gameObject);
+            //    springScript.natural_length.Add(natLength); //Modify this (not precise)
+            //    OtherBlob.collided = true;
+
+            //} 
         }
     }
 }
